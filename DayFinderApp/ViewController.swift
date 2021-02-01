9d3116b7646c2c3afe35059e9fffbfc41789b9be
JanisMuiznieks <janis.muiznieks8@gmail.com>
2021-02-01 20:17:32 +0200

    day finder part 1

--- DayFinderApp/ViewController.swift
+++ DayFinderApp/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dayTextField: UITextField!
+    
+    @IBOutlet weak var monthTextField: UITextField!
+    
+    @IBOutlet weak var yearTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var findButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("ViewController first lunch")
     }
 
 
+    @IBAction func findWeekdayTapped(_ sender: Any) {
+        //calendar
+        //DateComponents()
+        //dateComponent.day =  dayTextField.text
+        //calendar.date(from: dateComponets
+        
+        //DateFormatter()
+        
+        //resultLabel.text = result
+        
+    }
 }
 
